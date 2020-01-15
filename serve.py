05bf2cb1716f55ca1c120db5a25e21996464f300
Gittenburg <gittenburg@fsinf.at>
2020-01-15 18:20:01 +0100

    fix and rewrite serve.py, create utils.py

--- serve.py
+++ serve.py
@@ -1,90 +1,71 @@
 #!/usr/bin/env python3
 import html
-import re
-import sys
-
-import bottle
-import requests
 import sqlite3
 
-import config
+import bottle
 
-SERVER = 'https://mattermost.fsinf.at'
-TOSS = 'https://toss.fsinf.at/api'
-VOWI = 'https://vowi.fsinf.at'
-CHANNEL_PREFIX = SERVER + '/w-inf-tuwien/channels/'
-VOWI_TEAMID = 'sswtb6oqciyyfmkibh6mjz479w'
+import utils
 
 @bottle.get('/')
 def index():
 	return f'<h1>mattermost.fsinf.at glue API</h1>'\
 			'<h2>Redirect to channel by course code</h2><pre>/channel_by_course_code/&lt;code></pre>'
 
-def _mm_channel_name(title_de):
-	title_de = title_de.replace('für Informatik und Wirtschaftsinformatik', '')
-	chname = title_de.lower().replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
-	chname = re.sub('[^a-zA-Z0-9_]', '-', chname)
-	chname = re.sub('-+', '-', chname)
-	chname = chname[:63]
-	chname = chname.strip('-')
-
-	# now even group-channels (len == 40) do weird shit. Thanks MM :/
-	# Heisen-Issue: for some reason MM uses sometimes the channel name as id if
-	# len == 26 gives and when switching between teams.
-	if len(chname) == 54 or len(chname) == 40 or len(chname) == 26:
-		chname = chname+'0'
-
-	return chname
-
-def mm_request(path, method='get', **kwargs):
-	return requests.request(method, SERVER + '/api/v4' + path, **kwargs,
-			headers={'Authorization': 'Bearer ' + config.MM_TOKEN})
+def _infrelated(catalog):
+	return catalog['subject_de'] in ('Informatik', 'Wirtschaftsinformatik')\
+		or catalog['name_de'] == 'Transferable Skills'
 
 @bottle.route('/channel_by_course_code/<code:re:[A-Z0-9]{6}>', method=('GET', 'POST'))
 def channel_by_course_code(code):
-	conn = sqlite3.connect('channels.db')
+	conn = sqlite3.connect(utils.DBFILE)
 	cur = conn.cursor()
 	cur.execute('SELECT name FROM channels WHERE code = ?', (code,))
 	channel = cur.fetchone()
-	if channel is None:
-		res = requests.get(TOSS + '/courses/' + code)
-		if res.status_code != 200:
-			return bottle.HTTPError(404, 'Course not found')
-		course = res.json()
 
-		chname = _mm_channel_name(course['name_de'])
-		cur.execute('SELECT 1 FROM channels WHERE name = ?', (chname,))
+	if channel is not None:
+		return bottle.redirect(utils.CHANNEL_PREFIX + channel[0])
 
-		if cur.fetchone() is not None:
+	res = utils.toss_get('/courses/' + code)
+	if res.status_code != 200:
+		return bottle.HTTPError(404, 'TOSS could not find this course')
+
+	course = res.json()
+	chname = utils.mm_channel_name(course['name_de'])
+
+	channel = utils.mm_request(f'/teams/{utils.VOWI_TEAMID}/channels/name/{chname}')
+
+	if channel.status_code == 200:
+		cur.execute('INSERT INTO channels (name, code) VALUES (?, ?)', (chname, code))
+		conn.commit()
+		return bottle.redirect(utils.CHANNEL_PREFIX + chname)
+
+	res = utils.toss_get(course['machine']['instanceof'])
+	res.raise_for_status()
+
+	if not any([_infrelated(gm['catalog']) for gm in res.json()]):
+		return bottle.HTTPError(403, 'mattermost.fsinf.at is only for informatics related courses'\
+				' and this course does not seem to be associated with'\
+				' Informatics, Business Informatics or the Transferable Skills.')
+
+	if bottle.request.method == 'GET':
+		return f'<p>There currently is no channel for <i>{html.escape(course["name_en"])}</i>,'\
+				' but you can create one:</p>'\
+			f'<form method=post><button>Create channel "{html.escape(chname)}"</button></form>'
+
+	elif bottle.request.method == 'POST':
+		res = utils.mm_request('/channels', method='post', json={
+			'team_id': utils.VOWI_TEAMID,
+			'name': chname,
+			'display_name': course['name_de'][:64],
+			'type': 'O'
+		})
+		if res.status_code == 201:
 			cur.execute('INSERT INTO channels (name, code) VALUES (?, ?)', (chname, code))
-			bottle.redirect(CHANNEL_PREFIX + chname)
-
-		channel = mm_request(f'/teams/{VOWI_TEAMID}/channels/name/{chname}')
-		if channel.status_code == 200:
-			bottle.redirect(CHANNEL_PREFIX + chname)
+			conn.commit()
+			return bottle.redirect(utils.CHANNEL_PREFIX + chname)
 		else:
-			res = requests.get(TOSS + course['machine']['instanceof'])
-			inf_related = [gm['catalog']['subject_de'] in ('Informatik', 'Wirtschaftsinformatik')
-				or gm['catalog']['name_de'] == 'Transferable Skills' for gm in  res.json()]
-			if not any(inf_related):
-				return 'mattermost.fsinf.at is only for informatics related courses and this course does not seem to be associated with Informatics, Business Informatics or the Transferable Skills.'
-			if bottle.request.method == 'GET':
-				return f'<p>There currently is no channel for <i>{html.escape(course["name_en"])}</i>, but you can create one:</p>'\
-					f'<form method=post><button>Create channel "{html.escape(chname)}"</button></form>'
-			else:
-				res = mm_request('/channels', method='post', json={
-					'team_id': VOWI_TEAMID,
-					'name': chname,
-					'display_name': course['name_de'][:64],
-					'type': 'O'
-				})
-				if res.status_code != 201:
-					print(res.json())
-					return 'Channel creation failed'
-				else:
-					return bottle.redirect(CHANNEL_PREFIX + chname)
-	else:
-		bottle.redirect(CHANNEL_PREFIX + channel['name'])
+			print(res.json())
+			return bottle.HTTPError(500, 'Channel creation failed')
 
 if __name__ == '__main__':
 	bottle.run()
---
+++ utils.py
@@ -0,0 +1,39 @@
+import re
+
+import requests
+
+import config
+
+_MATTERMOST = 'https://mattermost.fsinf.at'
+_MATTERMOST_API = _MATTERMOST + '/api/v4'
+_TOSS_API = 'https://toss.fsinf.at/api'
+_VOWI_API = 'https://vowi.fsinf.at/api.php'
+_USER_AGENT = 'MM-Glue (https://github.com/fsinf/mm-glue)'
+
+CHANNEL_PREFIX = _MATTERMOST + '/w-inf-tuwien/channels/'
+VOWI_TEAMID = 'sswtb6oqciyyfmkibh6mjz479w'
+DBFILE = 'channels.db'
+
+def mm_channel_name(title_de):
+	title_de = title_de.replace('für Informatik und Wirtschaftsinformatik', '')
+	chname = title_de.lower().replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue')
+	chname = re.sub('[^a-zA-Z0-9_]', '-', chname)
+	chname = re.sub('-+', '-', chname)
+	chname = chname[:63]
+	chname = chname.strip('-')
+
+	# now even group-channels (len == 40) do weird shit. Thanks MM :/
+	# Heisen-Issue: for some reason MM uses sometimes the channel name as id if
+	# len == 26 gives and when switching between teams.
+	if len(chname) == 54 or len(chname) == 40 or len(chname) == 26:
+		chname = chname + '0'
+
+	return chname
+
+def mm_request(path, method='get', **kwargs):
+	return requests.request(method, _MATTERMOST_API + path, **kwargs,
+			headers={'Authorization': 'Bearer ' + config.MM_TOKEN,
+				'User-Agent': _USER_AGENT})
+
+def toss_get(path, **kwargs):
+	return requests.get(_TOSS_API + path, **kwargs, headers={'User-Agent': _USER_AGENT})
